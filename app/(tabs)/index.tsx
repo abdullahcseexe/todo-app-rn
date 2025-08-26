3978930136e1a3d77497e4515f20c5a379df13a0
abdullahcseexe <abduladilcse.exe@gmail.com>
2025-08-26 23:22:41 +0530

    Add UI functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,58 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { StatusBar, View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router';
-import useTheme from '@/hooks/useTheme';
+import useTheme , { ColorScheme } from '@/hooks/useTheme';
+import { api } from '@/convex/_generated/api';
+import { useMutation, useQuery } from 'convex/react';
+import { FunctionReference } from 'convex/server';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { LinearGradient } from "expo-linear-gradient"
+import { createHomeStyles } from '@/styles/home.styles';
+import Header from '@/components/Header';
+import TodoInput from '@/components/TodoInput';
+
+
+
 
 const Index = () => {
-  const {toggleDarkMode} = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
+  const homeStyles = createHomeStyles(colors);
+  const todos = useQuery(api.todos.getTodos)
   return (
-    <View style={styles.container}>
-      <Text>Hi!! This is index page.</Text>
-      <TouchableOpacity onPress={toggleDarkMode}><Text>Toggle the Mode</Text></TouchableOpacity>
-    </View>
+    <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
+      <StatusBar barStyle={colors.statusBarStyle} backgroundColor={colors.bg} />
+      <SafeAreaView style={homeStyles.safeArea}>
+        <Header />
+        <TodoInput/>
+
+         {todos?.map(todo => (
+          <Text key={todo._id}>{todo.text}</Text>
+         ))}
+
+        <TouchableOpacity onPress={toggleDarkMode}>
+          <Text>Toggle the Mode</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
+
   )
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-})
+const createStyles = (colors: ColorScheme) => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      gap: 10,
+      backgroundColor: colors.bg,
+    },
+  });
+  return styles;
+};
 
 export default Index
+
+function useMutuation(addTodo: FunctionReference<"mutation", "public", { text: string; }, string & { __tableName: "todos"; }, string | undefined>) {
+  throw new Error('Function not implemented.');
+}
